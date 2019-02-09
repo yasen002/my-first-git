ecd8625718317447e3cc50160311a42beb291b0f
Yasen <3622krim@gmail.com>
2019-02-09 10:42:01 -0800

    Added the comment to app.js. from YouTube vedio: Git$GitHub for beginners by traversy media

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+// create a html and a JS file
+// Bash:: git init
+// add your user anme and email to git :  Bash:: git config --global user.name 'Brad Traversy'
+// Bash:: git config --global user.email 'yasen@gmail.com' 
+// add html file to git repository:   git add index.html 
+// remove the tracked file:  Bash:: git rm --cashed 'index.html'
+// add any html files:  Bash:: git add *.html
+// add every file Bash:: git add .
+// Bash:: git commit    enter i to got insert mode 
+// put your comment and Bash::  :wq      to exit.
+// or     Bash:: git commit -m Your comment here  
+// create a ignore file:  .gitignore     put the file name that you want to ignore inside of that file
+// ignore a folder or file type: inside .gitignore file: /folderNmae    or   *txt(filel type)
+// to switch branch:  Bash:: git checkout branchName 
+// merge two branch:  Bash:: git merge branchName
+
+// remote repository:
+// open github create new repository.
+// follow the instruction to create new repository on the command line
+// once you added your file to the remote reository, {Bash:: git push} to update it
+
 console.log('Hello world');
 console.logI('I am trying to know how commit works.')
