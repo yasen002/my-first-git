ec6e38675ced1ed41d0de1a25faef18ea17c5701
Yasen <362yasen2@gmail.com>
2020-02-16 12:57:06 -0800

    new text

--- app.js
+++ app.js
@@ -21,3 +21,33 @@
 
 console.log('Hello world');
 console.logI('I am trying to know how commit works.')
+
+
+
+// for more info checkout this website
+// src: https://www.atlassian.com/git/tutorials/using-branches
+// The git fetch command downloads commits, files, and refs from a remote repository into your local repo. Fetching is what you do when you want to see what everybody else has been working on. 
+
+// git fetch: will download the remote content but not update your local repo's working state, leaving your current work intact.
+
+
+// git pull is the more aggressive alternative, it will download the remote content for the active local branch and immediately execute git merge to create a merge commit for the new remote content. If you have pending changes in progress this will cause conflicts and kickoff the merge conflict resolution flow.
+
+//  git branch command will output a list of the local branch refs. Examining the contents of the /.git/refs/heads/ directory would reveal similar output.
+
+//  Remote branch refs live in the ./.git/refs/remotes/ directory. 
+// git branch -r
+
+// So, unlike SVN, synchronizing your local repository with a remote repository is actually a two-step process: fetch, then merge. The git pull command is a convenient shortcut for this process.
+
+// GIT Fetch:
+// 1.configure the remote repo using the git remote command.
+// 2.pass the remote name to git fetch to download the contents.
+// 3. integrate our work into our local working copy: git checkout command to checkout the newly downloaded remote branch.
+// 4. create a new local branch named local_feature_branch: 
+// git checkout -b local_feature_branch
+
+// Synchronize origin with git fetch:
+// 1.git fetch origin
+// git fetch gives you access to the entire branch structure of another repository.
+// 2.To see what commits have been added to the upstream master, you can run a git log using origin/master as a filter:  
